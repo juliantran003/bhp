78e693c5c7ff225a427857276e3083ff92d7027e
Julian Tran <juliantran003@gmail.com>
2023-12-28 21:22:45 +0100

    initial

---
+++ components/layout.js
@@ -0,0 +1,12 @@
+// IMPORTS
+import Header from "./header";
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <Header />
+
+      <main>{children}</main>
+    </>
+  );
+}
