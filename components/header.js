f51c1002e0941d6bc59403274b2d5f98d1bfe909
Julian Tran <juliantran003@gmail.com>
2024-01-07 18:52:09 +0000

    first landing page prototype

--- components/header.js
+++ components/header.js
@@ -1,3 +1,29 @@
+// Imports
+import Link from "next/link";
+import menu from "../public/JSON/menu.json";
+
 export default function Header() {
-  return <div></div>;
+  return (
+    <header className="header">
+      <div className="header_title">
+        <h1>
+          PRINT<br></br> AGAINST<br></br> APARTHEID
+        </h1>
+
+        <h2>
+          BOYCOTT<br></br> HEWLETT & PACKARD
+        </h2>
+      </div>
+
+      <menu>
+        {menu.map((option) => {
+          return (
+            <Link href={option.url} key={option.url}>
+              {option.title}
+            </Link>
+          );
+        })}
+      </menu>
+    </header>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,33 @@
+// Imports
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+// JSON
+import overview from "../public/JSON/overview.json";
+
 export default function Home() {
-  return <div>hi</div>;
+  const router = useRouter();
+  const [selected, setSelected] = useState(overview[0]);
+
+  return (
+    <div className="home">
+      <div>
+        <div className="home_menu">
+          {overview.map((overview) => {
+            return (
+              <button onClick={() => setSelected(overview)}>
+                {overview.title}
+              </button>
+            );
+          })}
+        </div>
+        <div className="overview">
+          <h2>{selected.title}</h2>
+          <div>
+            <p>{selected.text}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
