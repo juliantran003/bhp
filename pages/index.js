48b74a044a80f55df40a776178d26c2a3c139fc1
Julian Tran <juliantran003@gmail.com>
2024-01-08 00:17:30 +0000

    change menu to text interface

--- pages/index.js
+++ pages/index.js
@@ -4,30 +4,93 @@ import { useRouter } from "next/router";
 
 // JSON
 import overview from "../public/JSON/overview.json";
+import { MiddlewareNotFoundError } from "next/dist/shared/lib/utils";
 
 export default function Home() {
   const router = useRouter();
   const [selected, setSelected] = useState(overview[0]);
+  const [main, setMain] = useState("");
+
+  function reset() {
+    setMain("");
+    setSelected(overview[0]);
+  }
 
   return (
     <div className="home">
       <div>
         <div className="home_menu">
-          {overview.map((overview) => {
-            return (
+          <button className={main === "overview" ? "clicked" : ""}>
+            {main === "overview" ? (
+              <span
+                onClick={() =>
+                  main === "overview" ? reset() : setMain("overview")
+                }
+              >
+                ⎖
+              </span>
+            ) : (
+              <></>
+            )}
+            &nbsp;
+            <span
+              onClick={() =>
+                main === "overview" ? reset() : setMain("overview")
+              }
+            >
+              {overview[0].title}
+            </span>
+          </button>
+          {overview.slice(1).map((overview) => {
+            return main === "overview" ? (
               <button
                 key={overview.title}
                 onClick={() => setSelected(overview)}
-                className={selected.title === overview.title ? "clicked" : ""}
+                className={
+                  selected.title === overview.title
+                    ? "clicked smaller"
+                    : "smaller"
+                }
               >
                 {overview.title}
               </button>
+            ) : (
+              <></>
             );
           })}
+          {main === "overview" ? (
+            <></>
+          ) : (
+            <button
+              className={main === "impact" ? "clicked" : ""}
+              onClick={() => setMain("impact")}
+            >
+              Impact
+            </button>
+          )}
+          {main === "overview" ? (
+            <></>
+          ) : (
+            <button
+              className={main === "pledge" ? "clicked" : ""}
+              onClick={() => setMain("pledge")}
+            >
+              Pledge
+            </button>
+          )}
         </div>
         <div className="overview">
           <h2>Statement</h2>
-          <div dangerouslySetInnerHTML={{ __html: selected.text }}></div>
+
+          {main === "" ? (
+            <div dangerouslySetInnerHTML={{ __html: selected.text }}></div>
+          ) : main === "overview" ? (
+            <div dangerouslySetInnerHTML={{ __html: selected.text }}></div>
+          ) : main === "impact" ? (
+            <p></p>
+          ) : (
+            <p></p>
+          )}
         </div>
       </div>
     </div>
